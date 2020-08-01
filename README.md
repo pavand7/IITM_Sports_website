2573c5bc63c725e8af388264184a032e083b349e
ppcool1163 <prashantppmsd2405@gmail.com>
2020-08-02 00:27:18 +0530

    IITM Sports Website

---
+++ README.md
@@ -0,0 +1,7 @@
+# IITM Sports Page
+
+## Features added:
+
+- Changed scrolltop feature to orange color
+- Integrated "animate on css" animation library to the code
+- Made navbar hide on scroll down and unhide on scroll up
---
+++ app.js
@@ -0,0 +1,152 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// home
+app.get("/", function (req, res) {
+  res.render("pages/home");
+});
+
+
+// EVENTS
+app.get('/events',function(req,res){
+  res.render('pages/events')
+})
+
+// Sports
+
+// inter-iit sports
+app.get("/interiit", function (req, res) {
+  res.render("pages/sports/interiit");
+});
+
+app.get("/athletics", function (req, res) {
+  res.render("pages/sports/interiit-sports/athletics");
+});
+
+app.get("/aquatics", function (req, res) {
+  res.render("pages/sports/interiit-sports/aquatics");
+});
+
+app.get("/badminton", function (req, res) {
+  res.render("pages/sports/interiit-sports/badminton");
+});
+
+app.get("/basketball", function (req, res) {
+  res.render("pages/records/interiit-sports/basketball");
+});
+
+app.get("/chess", function (req, res) {
+  res.render("pages/sports/interiit-sports/chess");
+});
+
+app.get("/cricket", function (req, res) {
+  res.render("pages/sports/interiit-sports/cricket");
+});
+
+app.get("/football", function (req, res) {
+  res.render("pages/sports/interiit-sports/football");
+});
+
+app.get("/hockey", function (req, res) {
+  res.render("pages/sports/interiit-sports/hockey");
+});
+
+app.get("/squash", function (req, res) {
+  res.render("pages/sports/interiit-sports/squash");
+});
+
+app.get("/tennis", function (req, res) {
+  res.render("pages/sports/interiit-sports/tennis");
+});
+
+app.get("/tabletennis", function (req, res) {
+  res.render("pages/sports/interiit-sports/tabletennis");
+});
+
+app.get("/volleyball", function (req, res) {
+  res.render("pages/sports/interiit-sports/volleyball");
+});
+
+app.get("/weightlifting", function (req, res) {
+  res.render("pages/sports/interiit-sports/weightlifting");
+});
+
+
+// non-interiit Sports
+app.get('/non-interiit', function(req,res){
+  res.render('pages/sports/non-interiit')
+});
+
+// Facilities/Grounds
+app.get('/facilities', function(req,res){
+  res.render('pages/sports/facilities')
+})
+
+
+// HISTORY
+
+// inter-IIT
+app.get('/history-interiit', function(req,res){
+  res.render('pages/history/interiit')
+});
+
+// IITM
+app.get('/history-iitm', function(req,res){
+  res.render('pages/history/iitm')
+});
+
+
+// RECORDS
+
+// Inter-IIT
+app.get('/records-interiit', function(req,res){
+  res.render('pages/records/interiit')
+});
+
+// Schroeter
+app.get('/records-schroeter', function(req,res){
+  res.render('pages/records/schroeter')
+});
+
+// Dean's Trophy
+app.get('/records-deanstrophy', function(req,res){
+  res.render('pages/records/deanstrophy')
+});
+
+
+// PEOPLE
+
+// Hall of fame
+app.get('/halloffame', function(req,res){
+  res.render('pages/people/wallOfFame')
+})
+
+// SOC
+app.get("/SOC", function (req, res) {
+  res.render("pages/people/SOC");
+});
+
+// Gymkhana
+app.get("/gymkhana", function (req, res) {
+  res.render("pages/people/Gymkhana");
+});
+
+
+// FEEDBACK
+app.get('/feedback', function(req,res){
+  res.render('pages/Feedback.ejs')
+})
+
+app.listen(PORT, function () {
+  console.log("Server is running on localhost", PORT);
+});
